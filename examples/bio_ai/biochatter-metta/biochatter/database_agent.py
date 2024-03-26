ba31f5845f79f54e9767786c3ce0c5b93ac11b06
wendecoder <wenonezra258@gmail.com>
2024-03-26 11:44:43 +0000

    added biochatterMetta repo

---
+++ examples/bio_ai/biochatter-metta/biochatter/database_agent.py
@@ -0,0 +1,93 @@
+import json
+from .prompts import BioCypherPromptEngine
+import neo4j_utils as nu
+from langchain.schema import Document
+
+
+class DatabaseAgent:
+    def __init__(
+        self,
+        model_name: str,
+        connection_args: dict,
+        schema_config_or_info_dict: dict,
+        conversation_factory: callable,
+    ) -> None:
+        """
+        Create a DatabaseAgent analogous to the VectorDatabaseAgentMilvus class,
+        which can return results from a database using a query engine. Currently
+        limited to Neo4j for development.
+
+        Args:
+            connection_args (dict): A dictionary of arguments to connect to the
+                database. Contains database name, URI, user, and password.
+
+            conversation_factory (callable): A function to create a conversation
+                for creating the KG query.
+        """
+        self.prompt_engine = BioCypherPromptEngine(
+            model_name=model_name,
+            schema_config_or_info_dict=schema_config_or_info_dict,
+            conversation_factory=conversation_factory,
+        )
+        self.connection_args = connection_args
+        self.driver = None
+
+    def connect(self) -> None:
+        """
+        Connect to the database and authenticate.
+        """
+        db_name = self.connection_args.get("db_name")
+        uri = f"{self.connection_args.get('host')}:{self.connection_args.get('port')}"
+        uri = uri if uri.startswith("bolt://") else "bolt://" + uri
+        user = self.connection_args.get("user")
+        password = self.connection_args.get("password")
+        self.driver = nu.Driver(
+            db_name=db_name or "neo4j",
+            db_uri=uri,
+            user=user,
+            password=password,
+        )
+
+    def is_connected(self) -> bool:
+        return not self.driver is None
+
+    def get_query_results(self, query: str, k: int = 3) -> list[Document]:
+        """
+        Generate a query using the prompt engine and return the results.
+        Replicates vector database similarity search API. Results are returned
+        as a list of Document objects to align with the vector database agent.
+
+        Args:
+            query (str): A query string.
+
+            k (int): The number of results to return.
+
+        Returns:
+            list[Document]: A list of Document objects. The page content values
+                are the literal dictionaries returned by the query, the metadata
+                values are the cypher query used to generate the results, for
+                now.
+        """
+        cypher_query = self.prompt_engine.generate_query(query)
+        # TODO some logic if it fails?
+        results = self.driver.query(query=cypher_query)
+
+        documents = []
+        # return first k results
+        # returned nodes can have any formatting, and can also be empty or fewer
+        # than k
+        if results is None or len(results) == 0 or results[0] is None:
+            return []
+        for result in results[0]:
+            documents.append(
+                Document(
+                    page_content=json.dumps(result),
+                    metadata={
+                        "cypher_query": cypher_query,
+                    },
+                )
+            )
+            if len(documents) == k:
+                break
+
+        return documents
---
+++ examples/bio_ai/biochatter-metta/biochatter/rag_agent.py
@@ -0,0 +1,145 @@
+from typing import Optional, List
+
+
+class RagAgentModeEnum:
+    VectorStore = "vectorstore"
+    KG = "kg"
+
+
+class RagAgent:
+    def __init__(
+        self,
+        mode: str,
+        model_name: str,
+        connection_args: dict,
+        n_results: Optional[int] = 3,
+        use_prompt: Optional[bool] = False,
+        schema_config_or_info_dict: Optional[dict] = None,
+        conversation_factory: Optional[callable] = None,
+        embedding_func: Optional[object] = None,
+        documentids_workspace: Optional[List[str]] = None,
+    ) -> None:
+        """
+        Create a RAG agent that can return results from a database or vector
+        store using a query engine.
+
+        Args:
+            mode (str): The mode of the agent. Either "kg" or "vectorstore".
+
+            model_name (str): The name of the model to use.
+
+            connection_args (dict): A dictionary of arguments to connect to the
+                database or vector store. Contains database name (in case of
+                multiple DBs in one DBMS), host, port, user, and password.
+
+            n_results: the number of results to return for method
+                generate_response
+
+            use_prompt (bool): Whether to use the prompt for the query. If
+                False, will not retrieve any results and return an empty list.
+
+            schema_config_or_info_dict (dict): A dictionary of schema
+                information for the database. Required if mode is "kg".
+
+            conversation_factory (callable): A function used to create a
+                conversation for creating the KG query. Required if mode is
+                "kg".
+
+            embedding_func (object): An embedding function. Required if mode is
+                "vectorstore".
+
+            documentids_workspace (Optional[List[str]], optional): a list of
+                document IDs that defines the scope within which similarity
+                search occurs. Defaults to None, which means the operations will
+                be performed across all documents in the database.
+
+        """
+        self.mode = mode
+        self.model_name = model_name
+        self.use_prompt = use_prompt
+        self.n_results = n_results
+        self.documentids_workspace = documentids_workspace
+        self.last_response = []
+        if self.mode == RagAgentModeEnum.KG:
+            from .database_agent import DatabaseAgent
+
+            if not schema_config_or_info_dict:
+                raise ValueError("Please provide a schema config or info dict.")
+            self.schema_config_or_info_dict = schema_config_or_info_dict
+
+            self.agent = DatabaseAgent(
+                model_name=model_name,
+                connection_args=connection_args,
+                schema_config_or_info_dict=self.schema_config_or_info_dict,
+                conversation_factory=conversation_factory,
+            )
+
+            self.agent.connect()
+
+            self.query_func = self.agent.get_query_results
+
+        elif self.mode == RagAgentModeEnum.VectorStore:
+            from .vectorstore_agent import VectorDatabaseAgentMilvus
+
+            if not embedding_func:
+                raise ValueError("Please provide an embedding function.")
+
+            self.agent = VectorDatabaseAgentMilvus(
+                embedding_func=embedding_func,
+                connection_args=connection_args,
+            )
+
+            self.agent.connect()
+
+            self.query_func = self.agent.similarity_search
+        else:
+            raise ValueError(
+                "Invalid mode. Choose either 'kg' or 'vectorstore'."
+            )
+
+    def generate_responses(self, user_question: str) -> list[tuple]:
+        """
+        Run the query function according to the mode and return the results in a
+        uniform format (list of tuples, where the first element is the text for
+        RAG and the second element is the metadata).
+
+        Args:
+            user_question (str): The user question.
+
+        Returns:
+            results (list[tuple]): A list of tuples containing the results.
+
+        Todo:
+            Which metadata are returned?
+        """
+        self.last_response = []
+        if not self.use_prompt:
+            return []
+        if self.mode == RagAgentModeEnum.KG:
+            results = self.query_func(user_question, self.n_results)
+            response = [
+                (
+                    result.page_content,
+                    result.metadata,
+                )
+                for result in results
+            ]
+        elif self.mode == RagAgentModeEnum.VectorStore:
+            results = self.query_func(
+                user_question,
+                self.n_results,
+                doc_ids=self.documentids_workspace,
+            )
+            response = [
+                (
+                    result.page_content,
+                    result.metadata,
+                )
+                for result in results
+            ]
+        else:
+            raise ValueError(
+                "Invalid mode. Choose either 'kg' or 'vectorstore'."
+            )
+        self.last_response = response
+        return response
